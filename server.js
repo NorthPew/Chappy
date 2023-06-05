88dc84ccb2c944589a52f8e8346da48fcfa546af
NorthPew <vbergstrom02@gmail.com>
2023-06-05 16:47:14 +0200

    Added middlewares, listen on port, directory path and where routes shall be

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
 
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
 // Configuration
 const PORT = 666
 const SECRET = 'Äppelpaj'
@@ -14,4 +17,17 @@ app.use((req, res, next) => {
     next();
 })
 
+// Directory
+const weAreHere = dirname(fileURLToPath(import.meta.url))
+const dist = join(weAreHere, './dist')
+app.use(express.static(dist))
+
 // Routes
+app.get('*', (req, res) => {
+    res.sendFile(join(dist, 'index.html'))
+})
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
