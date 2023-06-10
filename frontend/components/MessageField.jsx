cdc43ee0abcaf574aac0d0546023c99c6fb52651
NorthPew <vbergstrom02@gmail.com>
2023-06-10 15:44:03 +0200

    Now you can send messages using the app! Need to change sender later

--- frontend/components/MessageField.jsx
+++ frontend/components/MessageField.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { UserContext } from "../ContextRoot";
 import styled from "styled-components";
+import { sendMessage } from "../data/sendMessage";
+import { useState } from "react";
 
 const MessageWrapper = styled.div`
     width: calc(100% - 254px);
@@ -26,6 +28,10 @@ const MessageInput = styled.input`
 function MessageField() {
     const {isLoggedIn, selectSpecificView} = useContext(UserContext);
 
+    const [messageContent, setMessageContent] = useState("")
+
+
+    // Specific view
     let onSpecificView;
 
     if (selectSpecificView.route === 'chappy' && selectSpecificView.channel === 'one') {
@@ -34,13 +40,43 @@ function MessageField() {
         onSpecificView = 'Private Chat'
     }
 
-    return (
-    <MessageWrapper>
-        { isLoggedIn ?
-            <MessageInput type="text" placeholder={`Send a message to ${onSpecificView}`} ></MessageInput>
-            : <MessageInput type="text" disabled placeholder="You need to be logged in to send messages"></MessageInput>
+
+    function handleOnChangeMessageField(e) {
+        setMessageContent(e.target.value)
+    }
+
+    // Handlers
+    function handleOnSubmit(event) {
+        event.preventDefault()
+
+        let currentDate = new Date().toJSON().slice(0, 10);
+        let currentTime = new Date(new Date().getTime() + 1*60*60).toLocaleTimeString();
+
+        let newMessage = {
+            content: messageContent,
+            time: currentTime,
+            date: currentDate,
+            sender: [{
+                "id": 8144,
+                "username": "Hajime"
+            }]
         }
-    </MessageWrapper>
+        sendMessage(selectSpecificView.route, selectSpecificView.channel, newMessage)
+
+        console.log(newMessage);
+    }
+
+
+    return (
+    <form onSubmit={handleOnSubmit}>
+        <MessageWrapper>
+            { isLoggedIn ?
+                <MessageInput type="text" placeholder={`Send a message to ${onSpecificView}`} onChange={handleOnChangeMessageField} value={messageContent} ></MessageInput>
+                : <MessageInput type="text" disabled placeholder="You need to be logged in to send messages"></MessageInput>
+            }
+        </MessageWrapper>
+    </form>
+
 
     )
 }
