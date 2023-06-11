512144c7c13d592ac4e5a542594a4b489c2f45cf
NorthPew <vbergstrom02@gmail.com>
2023-06-11 19:59:11 +0200

    Made some changes to pagelink and so on

--- frontend/components/ChannelsOrFriends.jsx
+++ frontend/components/ChannelsOrFriends.jsx
@@ -18,7 +18,7 @@ const Panel = styled.div`
     flex-flow: column wrap;
     row-gap: 7.5px;
     background-color:#282b30;
-    color: #424549;
+    color: #666a70;
     position: relative;
 `
 
@@ -30,14 +30,42 @@ const PanelLink = styled(NavLink)`
     padding: .25em .75em;
     border-radius: 7.5px;
     width: 150px;
+    color: #666a70;
+    font-weight: 600;
 
     &.active {
         background-color: #424549;
         color: #f1f1f1;
     }
+
+    &:hover {
+        background-color: #424549;
+        color: #f1f1f1;
+    }
 `
 
-function ChannelsOrFriends(findGroup) {
+const DisabledPanelLink = styled.p`
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    text-decoration: none;
+    padding: .25em .75em;
+    border-radius: 7.5px;
+    width: 150px;
+    color: #666a70;
+    font-weight: 600;
+`
+
+const GroupTitleBox = styled.div`
+    border-bottom: 2.5px solid #1e2124;
+`
+
+const GroupTitle = styled.h1`
+    font-size: 22px;
+    font-weight: 600;
+`
+
+function ChannelsOrFriends({findGroup}) {
     const {isLoggedIn, isOnGroup} = useContext(UserContext);
 
     const allChannels = useLoaderData()
@@ -49,18 +77,34 @@ function ChannelsOrFriends(findGroup) {
             {
                 isOnGroup ?
                 <>
-                <p>{findGroup.title}</p>
+                <GroupTitleBox><GroupTitle>{findGroup.title}</GroupTitle></GroupTitleBox>
                     {
-                        
+                        !isLoggedIn ?
                         allChannels.map((group) => (
                             <>
                                 {Object.values(group.channels).flat().map((channel) => (
-                                    <PanelLink key={channel.id} to={`/group/${group.name}/channel/${channel.id}`}>
-                                        {channel.title}</PanelLink>
+                                    channel.public ? (
+                                        <PanelLink key={channel.id} to={`/group/${group.name}/channel/${channel.id}`}>
+                                            {channel.title}
+                                        </PanelLink>
+                                    ) : (
+                                        <DisabledPanelLink key={channel.id}>
+                                            {`${channel.title} 🔒`}
+                                        </DisabledPanelLink>
+                                    )
                                 ))}
                             </>
                         ))
-                    }
+                    : allChannels.map((group) => (
+                        <>
+                            {Object.values(group.channels).flat().map((channel) => (
+                                    <PanelLink key={channel.id} to={`/group/${group.name}/channel/${channel.id}`}>
+                                        {channel.title}
+                                    </PanelLink>
+                                )
+                            )}
+                        </>
+                    )) }
                 </> : <p>Friend 1</p>
             }
         </Panel>
